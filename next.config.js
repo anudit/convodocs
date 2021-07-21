1937d5e89ccb32328a5e1c83eed3ba1bcfe3bca6
Anudit Nagar <nagaranudit@gmail.com>
2021-07-21 18:35:04 +0530

    Add back PWA

--- next.config.js
+++ next.config.js
@@ -1,2 +1,29 @@
-const withNextra = require('nextra')('nextra-theme-docs', './theme.config.js')
-module.exports = withNextra()
+const withPWA = require('next-pwa')
+const withPlugins = require('next-compose-plugins')
+const runtimeCaching = require('next-pwa/cache')
+const { PHASE_PRODUCTION_BUILD, PHASE_PRODUCTION_SERVER } = require('next/constants')
+
+const withNextra = require('nextra')({
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.js',
+});
+
+const nextConfig = {
+    target: "experimental-serverless-trace",
+    exportPathMap: true,
+    poweredByHeader: false,
+    reactStrictMode: true,
+    experimental: {
+        optimizeCss: true
+    },
+}
+
+module.exports = withPlugins([
+    [withPWA, {
+      pwa: {
+        dest: 'public',
+        runtimeCaching
+      },
+    }, [PHASE_PRODUCTION_BUILD, PHASE_PRODUCTION_SERVER]],
+    [withNextra]
+], nextConfig)
