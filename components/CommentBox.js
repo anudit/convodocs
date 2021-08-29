4cdcc7c16fa3b62acf10b0694e8bad6f772f539c
Anudit Nagar <nagaranudit@gmail.com>
2021-08-29 23:57:11 +0530

    Improve Docs

--- components/CommentBox.js
+++ components/CommentBox.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CommentBox = ({threadId}) => {
   return (
-    <iframe src={`https://theconvo.space/embed/dt?url=https%3A%2F%2Fdocs.theconvo.space%2F&threadId=${threadId}`} allowtransparency="true" width="100%" height="auto" style={{marginTop:'10px'}} loading='eager'>
+    <iframe src={`http://localhost:3000/embed/dt?url=https%3A%2F%2Fdocs.theconvo.space%2F&threadId=${threadId}&height=300`} allowtransparency="true" width="100%" height="350px" style={{marginTop:'10px'}} loading='eager'>
       Comments
     </iframe>
   )
