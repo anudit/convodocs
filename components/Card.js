f2b347cbe2bf94e031160af0a1586961a8dcf1ba
Anudit Nagar <nagaranudit@gmail.com>
2021-11-17 22:53:15 +0530

    Update Card.js

--- components/Card.js
+++ components/Card.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ArticleCard = ({link, title, description, image}) => {
   return (
     <a href={link} style={{"textDecoration":"none"}} target="_blank">
-        <article style={{"display":"flex","flex-direction":"column","border-radius":"10px","border-width":"1px","width":"270px", "height":"310px"}} className="card">
+        <article style={{"display":"flex","flexDirection":"column","border-radius":"10px","border-width":"1px","width":"270px", "height":"310px"}} className="card">
             <img src={image} style={{"width":"100%","height":"152px","borderTopLeftRadius":"10px","borderTopRightRadius":"10px"}}/>
             <h4 style={{"padding":"10px","width":"100%","marginTop":"0px", "fontWeight":"900", "lineHeight":"20px"}}>
                 {title}
