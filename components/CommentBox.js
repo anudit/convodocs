43441c023f0269049361c73342edbdda26182bb1
Anudit Nagar <nagaranudit@gmail.com>
2022-02-01 10:29:43 +0530

    Update CommentBox.js

--- components/CommentBox.js
+++ components/CommentBox.js
@@ -20,9 +20,10 @@ const CommentBox = ({threadId}) => {
   }
   else{
     return (
-      <iframe src={`https://theconvo.space/embed/dt?url=https%3A%2F%2Fdocs.theconvo.space%2F&threadId=${threadId}&height=300`} allowtransparency="true" width="100%" height="350px" style={{marginTop:'10px'}} loading='eager'>
-        Comments
-      </iframe>
+      <></>
+      // <iframe src={`https://theconvo.space/embed/dt?url=https%3A%2F%2Fdocs.theconvo.space%2F&threadId=${threadId}&height=300`} allowtransparency="true" width="100%" height="350px" style={{marginTop:'10px'}} loading='eager'>
+      //   Comments
+      // </iframe>
     )
   }
 }
