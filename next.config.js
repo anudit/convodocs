48be4f17ccd58e5587c05020b93f3ac7e11e9cf0
Anudit Nagar <nagaranudit@gmail.com>
2022-03-23 21:36:34 +0530

    ⚡Switch to Preact

--- next.config.js
+++ next.config.js
@@ -15,7 +15,19 @@ const withNextra = require("nextra")({
 
 const nextConfig = {
   poweredByHeader: false,
-  reactStrictMode: true
+  reactStrictMode: true,
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+
+    return config;
+  }
 }
 
 module.exports = withPlugins([
