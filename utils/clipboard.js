ec67d79213026e71a09e2d16137725802bd4b42f
Anudit Nagar <nagaranudit@gmail.com>
2022-07-06 17:54:05 +0530

    Update

--- utils/clipboard.js
+++ utils/clipboard.js
@@ -5,7 +5,7 @@ export default function copyToClipboard(textToCopy, element = false) {
 
             if (element) element.innerHTML = `
             <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16">
-              <path fillRule="evenodd" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path>
+              <path fill="#0f0" fillRule="evenodd" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path>
             </svg>
             `;
 
