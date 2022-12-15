0c302b1efa5d3f102e71e1fd8f70207e5ff8212a
Anudit Nagar <nagaranudit@gmail.com>
2022-12-15 18:29:35 +0530

    Improve UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import { Inter } from '@next/font/google'
 import "../styles.css";
 
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} className={inter.className} />;
 }
